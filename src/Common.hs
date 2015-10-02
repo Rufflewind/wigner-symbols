6009bd26ec8a938d111a53133b2df597834eca2e
Phil Ruffwind <rf@rufflewind.com>
2015-10-02 02:59:37 -0400

    Fix Monoid

--- src/Common.hs
+++ src/Common.hs
@@ -1,20 +1,22 @@
 {-# LANGUAGE CPP #-}
 module Common
-  ( (<>)
+  ( Monoid
+  , (<>)
   , (<$>)
-  , Monoid
   , pure
   ) where
-
-#if MIN_VERSION_base(4, 5, 0)
-import Data.Monoid ((<>))
-#else
-import Data.Monoid (Monoid)
-#endif
 
 #if !MIN_VERSION_base(4, 8, 0)
 import Control.Applicative (pure)
 import Data.Functor ((<$>))
+#endif
+
+#if MIN_VERSION_base(4, 8, 0)
+import Data.Monoid ((<>))
+#elif MIN_VERSION_base(4, 5, 0)
+import Data.Monoid (Monoid, (<>))
+#else
+import Data.Monoid (Monoid, mappend)
 #endif
 
 #if !MIN_VERSION_base(4, 5, 0)
